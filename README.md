888edd8a13a4ba00058cea4b8c30f05a72f74033
Nick <n.assendelft@gmail.com>
2022-05-21 21:01:04 +0200

    Read from ANDROID_HOME env var

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Alfred workflow to start android emulators
 [Download v1.0.1](https://github.com/nassendelft/alfred-android-emulator/releases/download/1.0.1/Android.Emulator.v1.0.1.alfredworkflow)
 
 ## Configuration
+Picks up the `ANDROID_HOME` environment variable automatically if set.
+Can be overridden by setting the `emu_path` path variable (see screenshot below).
 ![Configuration](https://raw.githubusercontent.com/nassendelft/alfred-android-emulator/master/configure.png "configuration")
 
 ## Screenshot
--- emulist.py
+++ emulist.py
@@ -1,28 +1,67 @@
 from subprocess import check_output
-from xml.etree import ElementTree as etree
+from xml.etree import ElementTree
 from sys import argv
 from os import getenv
 from os import path
 
-path = path.expandvars(getenv('emu_path'))
 
-emulators = check_output([path, "-list-avds"]).decode("utf-8").rstrip().split('\n')
+def get_emulator_executable():
+    """
+    Reads the 'emu_path' environment variable
+    if not set it will look for the ANDROID_HOME env var instead.
+    """
+    emu_path_env = getenv('emu_path')
+    if emu_path_env is not None:
+        return path.expandvars(emu_path_env)
+
+    android_sdk_env = getenv('ANDROID_HOME')
+    if android_sdk_env is not None:
+        return path.join(path.expandvars(android_sdk_env), "emulator/emulator")
+
+    raise FileNotFoundError("Could not determine emulator executable location")
 
 
-def build_item(title):
-    item_el = etree.Element('item')
+def build_emulator_item(title):
+    item_el = ElementTree.Element('item')
     item_el.attrib = {'arg': title, 'type': 'file'}
-    title_el = etree.Element('title')
-    title_el.text = title
+    title_el = ElementTree.Element('title')
+    title_el.text = title.replace("_", " ")
     item_el.append(title_el)
     return item_el
 
-root = etree.Element('items')
-tree = etree.ElementTree(root)
 
-for emu in emulators:
-    if len(argv) == 1 or emu.startswith(argv[1]):
-        root.append(build_item(emu))
+def weigh_item(item, query):
+    """
+    Weighs items against given query
+    3 - query matches exactly
+    2 - items starts with the query
+    1 - the query has a partial match
+    0 - there's no match
+    """
+    return 3 if item == query else 2 if item.startswith(query) else 1 if query in item else 0
 
-print(etree.tostring(root, encoding='utf8', method='xml').decode())
 
+def search_by_query(items, query):
+    """
+    Searches for items that matches the query.
+    It filters out non matches and sorts it by
+    the weigh_item function
+    """
+    weighed = [(emu, weigh_item(emu, query)) for emu in items]
+    filtered = [emu for emu in weighed if emu[1] > 0]
+    sorts = sorted(filtered)
+    return [emu[0] for emu in sorts]
+
+
+def build_xml_tree(items):
+    root = ElementTree.Element('items')
+    for item in [build_emulator_item(element) for element in items]:
+        root.append(item)
+    return root
+
+
+emu_path = get_emulator_executable()
+emulators = check_output([emu_path, "-list-avds"]).decode("utf-8").rstrip().split('\n')
+sorted_items = emulators if len(argv) == 1 else search_by_query(emulators, argv[1])
+xml = ElementTree.tostring(build_xml_tree(sorted_items), encoding='utf8', method='xml').decode()
+print(xml)
